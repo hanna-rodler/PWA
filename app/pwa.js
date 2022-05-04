6985aa86a700a7e3cb545983264e8a673cbd26e2
hanna-rodler <haj.rodler@gmail.com>
2022-05-04 18:46:55 +0200

    render posts with pagination logic

--- app/pwa.js
+++ app/pwa.js
@@ -53,3 +53,50 @@ if (window.localStorage.getItem("token")) {
     )
   });
 }
+
+/*** PAGINATION***/
+
+fetch("https://api.s2010456026.student.kwmhgb.at/wp-json/wp/v2/posts?per_page=3")
+.then(function (response) {
+  paginate(response.headers.get("X-WP-TotalPages"));
+  return response;
+}).then(response => response.json())
+.then(posts => renderPosts(posts));
+
+function renderPosts(posts) {
+  // TODO: render posts like I want them to be rendered.
+  let posts_container = document.getElementById("posts");
+  for (let post of posts) {
+    let post_container = document.createElement("div");
+    post_container.classList.add("post");
+    post_container.dataset.resourceid = post.id;
+    let post_h3 = document.createElement("h3");
+    post_h3.innerHTML = post.title.rendered;
+    posts_container.append(post_h3);
+    let post_content = document.createElement("p");
+    post_content.innerHTML = post.content.rendered;
+    post_container.append(post_content);
+    posts_container.append(post_container);
+  }
+}
+
+function paginate(totalPages) {
+  //TODO: change Pagination a little bit.
+  if (totalPages > 1) {
+    let button = document.createElement("button");
+    button.innerHTML = "Mehr laden!";
+    button.id = "load_more_posts";
+    button.dataset.totalPages = totalPages;
+    button.dataset.nextPage = 2;
+    // button.removeEventListener("click");
+    button.addEventListener("click", function () {
+      fetch("https://api.s2010456026.student.kwmhgb.at/wp-json/wp/v2/posts?per_page=3&page=" +
+        this.dataset.nextPage).then(response => response.json())
+      .then(posts => {
+        renderPosts(posts);
+        button.dataset.nextPage++;
+      })
+    });
+    resources.append(button);
+  }
+}
