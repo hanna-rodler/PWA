138ee5f8dcbbe9585370a95a7d8cd45a0a62fa8a
hanna-rodler <haj.rodler@gmail.com>
2022-05-19 17:05:17 +0200

    added Logout to Profile

--- app/views/view.profile.js
+++ app/views/view.profile.js
@@ -8,9 +8,24 @@ import KWM_Route from '../js/kwm-route.js?v=0.2';
 
 export let view = new KWM_Route("/profile", async function(){
   await this.rendering();
+  btn_log_out.addEventListener("click", async function(e){
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("user_display_name");
+    // form_login.classList.remove("display")
+    login_state.classList.add("red");
+    user_display_name.innerHTML = " ";
+    form_login.style.display = "flex";
+    username.value="";
+    password.value="";
+  });
 });
 
 view.rendering = async function(){
   // kwm.templater.changeNavIcon("fa-user");
   await kwm.templater.renderTemplate("profile", document.getElementById("kwmJS"));
 };
+
+view.renderLoginForm = async function(){
+
+
+}
